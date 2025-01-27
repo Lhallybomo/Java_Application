3e7f2c285e47fb94af75d6faf981ac5e47636981
Bomote Olali <129884026+Lhallybomo@users.noreply.github.com>
2025-01-27 09:25:38 +0100

    Add files via upload

---
+++ CHAPTER 6/MultiDimensionalArraysEnhanced.java
@@ -0,0 +1,12 @@
+public class MultiDimensionalArraysEnhanced {
+    public static void main(String args[]) {
+        int[][] numbers = {{3, 4, 5}, {4, 5, 6}, {8, 9, 0}};
+
+        for (int[] row : numbers) { 
+            
+            for (int col : row) { 
+                System.out.println("Element: " + col); 
+            }
+        }
+    }
+}
